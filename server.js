77f5c7f5b8d2cc2e371125529122e979db3ba9ae
Lorelei Grennell <loreleigrennell@gmail.com>
2026-07-01 18:55:54 -0500

    Took IP address back out of app.listen(); was not supposed to be there

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use("/writing", writingRouter);
 app.use("/api", apiRouter);
 
 const port = process.env.PORT;
-app.listen(port, "3.143.243.28", () => {
-    console.log("server running on port 3000")
+app.listen(port, () => {
+    console.log("server running on port ${port}")
 });
