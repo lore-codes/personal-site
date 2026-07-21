d6764b97ad2bf1a11561ed368b6a56ce7b852873
Lorelei Grennell <loreleigrennell@gmail.com>
2026-07-21 18:31:37 -0500

    Clue scoreccard prototype tweak: removed width/height pixel count from heading; was being used for debugging

--- public/js/programming/clue-scorecard.js
+++ public/js/programming/clue-scorecard.js
@@ -6,6 +6,6 @@ function startGame() {
     let h2 = document.querySelector("h2");
     let width = document.querySelector("body").clientWidth;
     let height = document.querySelector("body").clientHeight;
-    h2.innerHTML = "Width: " + width + " Height: " + height;
+    /*h2.innerHTML = "Width: " + width + " Height: " + height;*/
     let clue = new Clue(width, height);
 }
